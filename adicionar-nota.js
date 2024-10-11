8a30e977a3d726c0f5cb033811793504338adc21
Lucas Barbosa <lucas.barbosa12397@gmail.com>
2024-10-11 02:32:44 -0300

    Feito até aula 2, atividade 02 Dividindo com slice()

--- adicionar-nota.js
+++ adicionar-nota.js
@@ -5,3 +5,5 @@ notas.push(7);
 const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
 
 console.log(media);
+
+// Fazer um loop que adicione todas as notas do array notas e calcule a média
